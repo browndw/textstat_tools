15235a5d0bf0aaff7fc7917c1a7084aeaf23821b
browndw <davidwestbrown@davidbrsmacbook.fios-router.home>
2019-11-10 09:20:59 -0500

    Update collocations_functions.R

--- functions/collocations_functions.R
+++ functions/collocations_functions.R
@@ -46,7 +46,7 @@ collocates_by_MI <- function(quant_tokens, node_word, left, right){
   col_freq <- merge(col_freq, totals[,1:2], by = "feature", all.x = T)
   
   # Find the frequency of our node word and the total corpus count.
-  node_freq <- as.numeric(totals[stringr::str_detect(totals$feature, paste0("^", node_word, "$")), 2])
+  node_freq <- as.numeric(totals[stringr::str_detect(totals$feature, regex(paste0("^", node_word, "$"), ignore_case = T)), 2])
   corpus_total <- sum(totals$frequency)
   
   # The function calculates the most common type of Mutual Information.
@@ -73,8 +73,82 @@ collocates_by_MI <- function(quant_tokens, node_word, left, right){
   col_freq$feature <- as.character(col_freq$feature)
   attr(col_freq, "node_word") <- node_word
   attr(col_freq, "corpus_total") <- corpus_total
+  col_freq <- structure(col_freq, class = c("collocations", "data.frame"))
   return(col_freq)
 }
 
+# This function operationalizes the idea of collcational networks described by
+# Brezina, McEnery & Wattam (2015):
+# https://www.jbe-platform.com/content/journals/10.1075/ijcl.20.2.01bre
+#
+# The function takes data.frames produced by the collocates_by_MI() function above
+# and generates a tidygraph data object for plotting in ggraph.
+col_network <- function(col_1, col_2, ...) {
+  
+  # put all the collocation data.frames into a list
+  all_col <- list(col_1, col_2, ...)
+  
+  # check the object class
+  #test_class <- lapply(all_col, class)
+  #if (unique(sapply(test_class, "[[", 1)) != "collocations") stop ("your data must be collocations objects")
+  
+  # extract the node words from attributes
+  node_words <- lapply(all_col, function(x) attr(x, "node_word"))
+  # extract the corpus totals from the attritubes
+  corpus_totals <- lapply(all_col, function(x) attr(x, "corpus_total"))
+  
+  # create an index
+  idx <- seq(1:length(all_col))
+  
+  # normalize frequencies
+  all_col <- lapply(idx, function(i) dplyr::mutate(all_col[[i]], col_freq = col_freq/corpus_totals[[i]]))
+  # add a column of node words by id
+  edges <- lapply(idx, function(i) cbind(node_word = i, all_col[[i]]))
+  # bind all data.frames
+  edges <- dplyr::bind_rows(edges)
+  
+  # create a vector of collocation tokens
+  col_vect <- lapply(idx, function(i) (all_col[[i]]$feature))
+  # get unique tokens
+  col_vect <- unique(unlist(col_vect))
+  # and sort alphabetically
+  col_vect <- sort(col_vect)
+  
+  # calculate the minimum id for collocations based on the number of node words
+  id_min <- max(idx) + 1
+  # calcuate the max id number
+  id_max <- length(col_vect) + (id_min - 1)
+  # generate ids for unique collocations
+  col_id <- data.frame(id = seq(id_min, id_max), feature = col_vect, stringsAsFactors = F)
+  # merge to create connections based on ids
+  edges <- merge(edges, col_id, by = "feature", all=T)
+  
+  # group all collocates by id
+  id_grp <- dplyr::group_by(edges, id)
+  # calculate the number of node words each collocation intersects with
+  intersects <- dplyr::tally(id_grp)
+  # for collocates with multiple intersections, summarize by mean
+  freq_norm <- dplyr::summarise(id_grp, freq = mean(col_freq))
+  # find the max frequency of all collocates
+  freq_max <- max(freq_norm$freq)
+  # make a data.frame of node words, settining intersections to 0 and frequency to freq_max
+  node_words <- data.frame(id = seq_along(idx), feature = unlist(node_words), n = 0, freq = freq_max)
+  
+  # merge values into a single data.frame, containing all node information
+  nodes <- Reduce(function(x, y) merge(x, y, by = "id"), list(col_id, intersects, freq_norm))
+  # add node_word values to top of data.frame
+  nodes <- rbind(node_words, nodes)
+  nodes$feature <- as.character(nodes$feature)
+  nodes$n <- as.factor(nodes$n)
+  nodes <- dplyr::rename(nodes, label = feature, n_intersects = n, node_weight = freq)
+  
+  # assemble edge values
+  edges <- dplyr::select(edges, id, node_word, MI_1)
+  edges <- dplyr::rename(edges, to = id, from = node_word, link_weight = MI_1)
+  
+  # generate a tidygraph object for plotting
+  col_net <- tidygraph::tbl_graph(nodes = nodes, edges = edges, directed = F)
+  return(col_net)
+}
 
 
